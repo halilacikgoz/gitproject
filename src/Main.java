8bf17c508dd3cb2a6ec0f5c9ffa77b419f6f78e4
halilacikgoz <halilacikgoz189@gmail.com>
2019-05-25 13:25:31 +0300

    4th branch

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("it is for another branch.");
         System.out.println("second message ");
-		    System.out.println("3rd message ");
+		System.out.println("3rd message ");
+		System.out.println("4th branch for merge ");
 
     }
 }
